eb6a4a6ec55246540cd6090da307556c154cfa0a
Eliezer Marques <neto15.cg@gmail.com>
2021-04-02 18:17:15 -0300

    Remove store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,5 @@
-import { wrapper } from '../store/store';
-
 function App({ Component, pageProps }: any) {
   return <Component {...pageProps} />;
 }
 
-export default wrapper.withRedux(App);
+export default App;
